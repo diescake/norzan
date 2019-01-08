446af5f000cb7c0ed94c85adf2464751a77347c1
diescake <daisuke.kondo@access-company.com>
2019-01-08 20:53:04 +0900

    Tutorial done

--- background.js
+++ background.js
@@ -2,4 +2,18 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color: '#3aa757' }, () => {
     console.log('The color is green')
   })
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {
+              hostEquals: 'developer.chrome.com',
+            },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ])
+  })
 })
---
+++ options.js
@@ -0,0 +1,17 @@
+const page = document.getElementById('buttonDiv')
+const BUTTON_COLORS = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1']
+
+const constructOptions = colors => {
+  colors.forEach(color => {
+    const button = document.createElement('button')
+    button.style.backgroundColor = color
+    button.addEventListener('click', () => {
+      chrome.storage.sync.set({ color }, () => {
+        console.log(`color is ${color}`)
+      })
+    })
+    page.appendChild(button)
+  })
+}
+
+constructOptions(BUTTON_COLORS)
