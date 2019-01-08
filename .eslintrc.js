a85c8544285c9907a832f2f90a61da589e98100a
diescake <daisuke.kondo@access-company.com>
2019-01-08 19:45:34 +0900

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  globals: {
+    chrome: true,
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': 'off',
+  },
+};
