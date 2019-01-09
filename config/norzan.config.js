ffd09254aa8cadd92d07fb371914d77cee390868
diescake <daisuke.kondo@access-company.com>
2019-01-09 16:25:16 +0900

    minor refactored

--- config/norzan.config.js
+++ config/norzan.config.js
@@ -2,8 +2,12 @@ export const norzanConfig = {
   durationFormat: 'hh:mm:ss.SS',
   updateIntervalMs: 90,
   workTimeStart: 10.5,
-  workTimeEnd: 19,
+  workTimeEnd: 19.0,
   wording: {
     workFinished: '本日の業務は終了しました。',
+  },
+  style: {
+    backgroundColor: '#222',
+    fontColor: '#e3e3e3',
   },
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-(async () => {
+const setStyle = style => {
+  const body = document.body
+  const time = document.getElementById('time')
+
+  body.style.backgroundColor = style.backgroundColor
+  time.style.color = style.fontColor
+}
+
+;(async () => {
   const { Norzan } = await import('/js/Norzan.js')
   const { norzanConfig } = await import('/config/norzan.config.js')
+
+  setStyle(norzanConfig.style)
 
   const time = document.getElementById('time')
   const norzan = new Norzan(norzanConfig, time)
