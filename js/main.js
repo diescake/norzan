38db50881fc1fa9eace7335ff5506827183f5f6c
diescake <daisuke.kondo@access-company.com>
2019-01-15 18:38:37 +0900

    Fetched option labels from norzan.config.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
 import '../style/reset.css'
 import '../style/common.css'
 import '../style/main.css'
+
 import { Norzan } from '../js/Norzan.js'
 import { defaultOption } from '../config/norzan.config.js'
 
 const loadOption = () =>
   new Promise(resolve => {
-    chrome.storage.sync.get(
-      ['durationFormat', 'updateIntervalMs', 'openingTime', 'closingTime', 'messageInClosed', 'backgroundColor', 'fontColor'],
-      data => {
-        if (!data.durationFormat) {
-          resolve(null)
-        }
-
-        resolve({
-          durationFormat: data.durationFormat,
-          updateIntervalMs: data.updateIntervalMs,
-          openingTime: data.openingTime,
-          closingTime: data.closingTime,
-          messageInClosed: data.messageInClosed,
-          backgroundColor: data.backgroundColor,
-          fontColor: data.fontColor,
-        })
+    chrome.storage.sync.get(Object.keys(defaultOption), data => {
+      if (!data.durationFormat) {
+        resolve(null)
       }
-    )
+
+      resolve({
+        durationFormat: data.durationFormat,
+        updateIntervalMs: data.updateIntervalMs,
+        openingTime: data.openingTime,
+        closingTime: data.closingTime,
+        messageInClosed: data.messageInClosed,
+        backgroundColor: data.backgroundColor,
+        fontColor: data.fontColor,
+      })
+    })
   })
 
 const setStyle = config => {
