3daa6c10c5c4076c1161e97484985945f670a82e
diescake <daisuke.kondo@access-company.com>
2019-02-05 22:53:43 +0900

    Change default times

--- config/norzan.config.js
+++ config/norzan.config.js
@@ -1,8 +1,8 @@
 export const defaultOption = {
   durationFormat: 'hh:mm:ss.SS',
   updateIntervalMs: 90,
-  openingTime: '10:30:00',
-  closingTime: '19:00:00',
+  openingTime: '09:00:00',
+  closingTime: '17:30:00',
   messageInClosed: 'During non-business hours.',
   backgroundColor: '#222222',
   fontColor: '#e3e3e3',
